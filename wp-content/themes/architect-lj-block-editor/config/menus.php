638b94128b2a34d8219836f6e32cf28c346405a4
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-01-27 13:06:21 -0500

    set configuration

--- wp-content/themes/architect-lj-block-editor/config/menus.php
+++ wp-content/themes/architect-lj-block-editor/config/menus.php
@@ -6,5 +6,8 @@ return [
      */
     'menus' => [
         'main-nav' => __('Main Navigation'),
+        'footer1' => __('Use Cases'),
+        'footer2' => __('Company'),
+        'footer3' => __('Legal'),
     ],
 ];
