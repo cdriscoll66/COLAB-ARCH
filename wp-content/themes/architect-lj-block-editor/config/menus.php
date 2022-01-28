82e3f305f78d19e5404f895d42a3487646644913
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-01-28 13:33:49 -0500

    menu and gradient configs

--- wp-content/themes/architect-lj-block-editor/config/menus.php
+++ wp-content/themes/architect-lj-block-editor/config/menus.php
@@ -5,9 +5,9 @@ return [
      * List of menus to register with WordPress during bootstrap
      */
     'menus' => [
-        'main-nav' => __('Main Navigation'),
-        'footer1' => __('Use Cases'),
-        'footer2' => __('Company'),
-        'footer3' => __('Legal'),
+        'main-nav'  => __('Main Navigation'),
+        'footer1'   => __('Footer 1'),
+        'footer2'   => __('Footer 2'),
+        'footer3'   => __('Footer 3'),
     ],
 ];
