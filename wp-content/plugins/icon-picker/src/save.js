19cb66118a46bcc7ff96e8ab19685f3e75cb9109
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-02-04 13:31:20 -0500

    iconpicker setup

---
+++ wp-content/plugins/icon-picker/src/save.js
@@ -0,0 +1,66 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
+
+import classnames from "classnames";
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps } from "@wordpress/block-editor";
+
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function save({ attributes }) {
+
+	const classStyle = (attributes.colabStyle ? "is-style-icon" : "");
+
+
+	const align = attributes.align;
+	const classes = classnames( {
+		[ `align${ align }` ]: align
+	});
+
+	if ( 'left' === align || 'right' === align || 'center' === align ) {
+	return (
+		<div {...useBlockProps.save()}>
+			<div className={ classes }>
+				<i
+					className={classnames(
+						"fal",
+						attributes.icons,
+						attributes.size,
+						classStyle
+					)}
+				></i>
+			</div>
+		</div>
+	);
+					}
+	return (
+	<div {...useBlockProps.save( { className: classes } )}>
+		<i
+			className={classnames(
+				"fal",
+				attributes.icons,
+				attributes.size,
+				classStyle
+							)}
+		></i>
+	</div>
+	);
+
+}
