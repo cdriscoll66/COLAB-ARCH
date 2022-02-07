505fdcc618261a693a4c733ede7c2f2fe489f2ce
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-02-07 13:32:05 -0500

    accordion panels animated

--- wp-content/themes/architect-lj-block-editor/assets/scripts/theme/accordion.js
+++ wp-content/themes/architect-lj-block-editor/assets/scripts/theme/accordion.js
@@ -7,10 +7,12 @@ function init(){
             parents.forEach(parent => {
                 let trigger = parent.querySelector('h3');
                 trigger.addEventListener('click', function (event) {
-                    parents.forEach(parent => {
-                        parent.classList.remove('open');
-                    });
+                    let openPanel = accordion.querySelector('.open');
+                    let panel = openPanel.querySelector('.wp-block-column');
+                    panel.style.maxHeight = '56px';
+                    openPanel.classList.remove('open');
                     event.target.closest('.wp-block-columns').classList.add('open');
+                    event.target.closest('.wp-block-column').style.maxHeight = null;
                 }, false);
             });
         });
