b5f2f5f316190456891b36cf452725957507bda3
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-02-17 14:33:24 -0500

    swap on icons

--- wp-content/themes/architect-lj-block-editor/config/preload-fonts.php
+++ wp-content/themes/architect-lj-block-editor/config/preload-fonts.php
@@ -5,7 +5,7 @@ function fontpreload()
 ?>
 
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700" as="font" display="swap" crossorigin="anonymous">
-    <link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons" as="font" crossorigin="anonymous">
+    <link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons" as="font" display="swap" crossorigin="anonymous">
 
 <?php
 };
