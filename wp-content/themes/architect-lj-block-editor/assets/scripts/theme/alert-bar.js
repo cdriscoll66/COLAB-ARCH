9c212ca37696d870c984ac804d30a6aa15837abf
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-03-14 14:00:01 -0400

    alert bar hides on mobile - also - uses epoch seconds instead to test time for safari

--- wp-content/themes/architect-lj-block-editor/assets/scripts/theme/alert-bar.js
+++ wp-content/themes/architect-lj-block-editor/assets/scripts/theme/alert-bar.js
@@ -9,58 +9,74 @@
  *
  * @link https://pantheon.io/docs/caching-advanced-topics/
  */
- import cookie from './Cookies';
+import cookie from "./Cookies";
 
- //check if browser is IE 11
- let isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+//check if browser is IE 11
+let isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+
+const $ = window.jQuery;
+const s = {
+    $window: $(window),
+    $alerts: $(".js-alert"),
+};
+
+function init() {
+    if (s.$alerts.length) {
+        const currentDate = Date.now();
+        const secondsSinceEpoch = Math.round(currentDate / 1000)
+
+        s.$alerts.each(function (index, element) {
+            const $element = $(element);
+            const startValue = element.getAttribute("data-start");
+            const endValue = element.getAttribute("data-end");
+
+            const alertId = element.getAttribute("data-id");
 
 
- const $ = window.jQuery;
- const s = {
-     $window : $(window),
-     $alerts : $('.js-alert'),
- };
+            const alertCookie = "STYXKEY-alert-" + alertId;
 
- function init() {
+            let isInDateRange = false;
 
-     if (s.$alerts.length) {
-         const currentDate = Date.now();
-         const nowDate     = new Date(currentDate);
-         s.$alerts.each(function(index, element) {
-             const $element    = $(element);
-             const startValue  = element.getAttribute('data-start');
-             const endValue    = element.getAttribute('data-end');
-             const alertId     = element.getAttribute('data-id');
-             const alertCookie = 'STYXKEY-alert-' + alertId;
-             let isInDateRange = false;
-             if (startValue && endValue) {
-                 const startDate = new Date(startValue);
-                 const endDate   = new Date(endValue);
-                 isInDateRange = startDate.getTime() <= nowDate.getTime() && nowDate.getTime() <= endDate.getTime();
-             }
-             else {
-                 isInDateRange = true;
-             }
 
-             if (isInDateRange) {
-                 if (cookie(alertCookie) === undefined || cookie(alertCookie) !== 'inactive' || isIE11 ) {
-                     $element.addClass('active').removeClass('inactive');
-                     cookie(alertCookie, 'active');
-                 }
-             }
-         });
-     }
+            if (startValue && endValue) {
 
-     events();
- }
+                const startDate = startValue;
+                const endDate = endValue;
 
- function events() {
+                console.log(startDate);
+                console.log('now'+currentDate);
+                console.log('end'+endDate);
+                isInDateRange = startDate <= secondsSinceEpoch && secondsSinceEpoch <= endDate;
 
-     $('.js-alert__dismiss').on('click', function( event ) { event.preventDefault();
-         const $this = $(this);
-         cookie('STYXKEY-alert-' + $this.parent().data('id'), 'inactive', { 'maxage' : 1 } );
-         $this.parent().addClass('inactive').removeClass('active');
-     });
- }
+            } else {
+                isInDateRange = true;
+            }
 
- export {init as default};
+            if (isInDateRange) {
+                if (
+                    cookie(alertCookie) === undefined ||
+                    cookie(alertCookie) !== "inactive" ||
+                    isIE11
+                ) {
+                    $element.addClass("active").removeClass("inactive");
+                    cookie(alertCookie, "active");
+                }
+            }
+        });
+    }
+
+    events();
+}
+
+function events() {
+    $(".js-alert__dismiss").on("click", function (event) {
+        event.preventDefault();
+        const $this = $(this);
+        cookie("STYXKEY-alert-" + $this.parent().data("id"), "inactive", {
+            maxage: 1,
+        });
+        $this.parent().addClass("inactive").removeClass("active");
+    });
+}
+
+export { init as default };
