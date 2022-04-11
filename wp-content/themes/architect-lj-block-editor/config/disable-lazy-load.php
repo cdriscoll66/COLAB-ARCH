f9ed770db5becb1e179585b414d1da35007301a4
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-04-11 16:03:24 -0400

    removed first image from lazy load

--- wp-content/themes/architect-lj-block-editor/config/disable-lazy-load.php
+++ wp-content/themes/architect-lj-block-editor/config/disable-lazy-load.php
@@ -1,2 +1,13 @@
 <?php
-add_filter( 'wp_lazy_loading_enabled', '__return_false' );
+function wphelp_no_lazy_load_id( $value, $image, $context ) {
+    if ( 'the_content' === $context ) {
+
+    $image_url = wp_get_attachment_image_url( 22, 'full' ); // Change the ID and size
+
+    if ( false !== strpos( $image, ' src="' . $image_url . '"' )) {
+    return false;
+    }
+    }
+    return $value;
+    }
+    add_filter( 'wp_img_tag_add_loading_attr', 'wphelp_no_lazy_load_id', 10, 3 );
