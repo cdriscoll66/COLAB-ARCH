cf87faca74455487d80266c340570e031c55c0e8
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-04-11 14:49:32 -0400

    font preload switch

--- wp-content/themes/architect-lj-block-editor/config/preload-fonts.php
+++ wp-content/themes/architect-lj-block-editor/config/preload-fonts.php
@@ -3,10 +3,15 @@
 function fontpreload()
 {
 ?>
-
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700" as="font" display="swap" crossorigin="anonymous">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700" as="style" display="swap" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
     <link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons" as="font" display="swap" crossorigin="anonymous">
-
+    <noscript>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700"
+        rel="stylesheet"
+        type="text/css"
+    />
+</noscript>
 <?php
 };
 add_action('wp_head', 'fontpreload');
